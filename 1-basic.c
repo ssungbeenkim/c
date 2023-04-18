cd8ed77565ede4e28801807c96566db2bb4b4b7d
wandakim <nwd1583@naver.com>
2023-04-18 22:42:46 +0900

    Edit description and review

--- 1-basic.c
+++ 1-basic.c
@@ -1,11 +1,11 @@
-#include <stdio.h> 
+#include <stdio.h> // 헤더파일을 포함한다.
 /* 
 #: preprocessor 
 include: 헤더파일을 포함한다. 
 studio.h: io 관련된 함수들이 있는 헤더파일
 */
 
-int main(void)
+int main(void) 
 {
 	printf("Hello World!\n"); // 줄바꿈
   printf("Nice Day!\t"); // tap
@@ -15,10 +15,12 @@ int main(void)
   char Letter = 'Y'; // 문자 '' 따옴표를 넣어야 한다. // 1byte 8bit 문자열은 ""
   double cost = 34.567;// 실수 // 8byte
 
-  printf("\n");
+  printf("\n"); 
+  // 서식문자 %d: 정수형, %c: 문자형, %f: 실수형, %g: 실수형
+  // %f와 %g의 차이는 소숫점 이하 자리수가 다르다. %f는 6자리, %g는 6자리 이하로 표현한다.
   printf("구구단은 %d 학년 때 외워요.\n",grade); // %d(decimal. 정수형 출력)에 3이 들어감 
   printf("알파벳은 %c입니다.\n",Letter);
-  printf("용돈은 달러로 %5.2f입니다.\n",cost); // 5자리, 소숫점 이하 2자리이하 반올림
+  printf("용돈은 달러로 %5.2f입니다.\n",cost); // 용돈은 달러로 34.57입니다. 5자리, 소숫점 이하 2자리이하 반올림
   printf("용돈은 달러로 %10.2f입니다.\n",cost); //용돈은 달러로      34.57입니다.
 
   int score = 30;
